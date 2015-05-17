8f79f0a3b11147a8100dc5085d0108ec3f92e309
avishekp4 <avishekp4@gmail.com>
2015-05-17 12:25:28 +0530

    ok

---
+++ app.php
@@ -0,0 +1,274 @@
+<?php
+include('mpdf/mpdf.php');
+$html='<!doctype html>
+<html>
+<head>
+	<title>HTML Editor - Full Version</title>
+</head>
+<body>
+<div>
+<h2>&nbsp;<span style="font-size:10px;"> Sl No.&nbsp;</span> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Kharagpur College&nbsp;</h2><img style="float:right" src="image_place_holder.png" height="155" width="125"/>
+
+<p style="text-align: center;"><strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ADMISSION FORM</strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;<strong>2015-2016</strong></p>
+    
+
+<p><strong>1 <sup>ST </sup>Year&nbsp;B.A./B.Sc./B.Com.</strong></p>
+
+<p>HONOURS/GENERAL&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;DAY/MORNING/EVENING -SHIFT</p>
+</div>
+
+<table align="center" border="1" cellpadding="1" cellspacing="1" style="width: 700px;">
+	<thead>
+		<tr>
+			<th colspan="5" scope="col">(For Office Use Only)</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td style="text-align: center;">Subject</td>
+			<td style="text-align: center;">Docket No</td>
+			<td style="text-align: center;">Merit Point</td>
+			<td style="text-align: center;">Date of Admission</td>
+			<td style="text-align: center;">College Roll No</td>
+		</tr>
+		<tr>
+			<td>&nbsp;</td>
+			<td>&nbsp;</td>
+			<td style="text-align: center;">124</td>
+			<td>&nbsp;</td>
+			<td>&nbsp;</td>
+		</tr>
+	</tbody>
+</table>
+
+<p>1. Name in Full (<span style="font-size:8px;">in Block Capitl Letters</span>):Avshekparu</p>
+
+<p>2. Date of Birth :&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;3.Sex : &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;4. Blood Group :&nbsp;</p>
+
+<p>5.a) Father&#39;s Name :&nbsp;</p>
+
+<p>&nbsp; &nbsp;b) Father&#39;s Education: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; c)Father&#39;s Occupation: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; d)Monthly&nbsp;Income:</p>
+
+<p>6.a) Mothers&#39;s Name :&nbsp;</p>
+
+<p>&nbsp; &nbsp;b) Mothers&#39;s&nbsp;Education: &nbsp; &nbsp; &nbsp; &nbsp; c)Mothers&#39;s&nbsp;Occupation: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; d )Monthly&nbsp;Income:</p>
+
+<p>7. a)Address for Communication:</p>
+
+<p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;PIN:- &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;.b)Telephone/Mobile No.</p>
+
+<p>8. a) Nationality:.............................................. &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;b) Religion: ...................</p>
+
+<p>9. Caste :<span style="font-size:9px;">( Certificate issued by the competent authority should be enclosed for SC/ST/OBC)</span>......................................................</p>
+
+<p><br />
+10. Whether admission sought in Physically Challenged / Sport&#39;sQuota <span style="font-size:9px;">(Relevant Certificates to be attached)</span>:.....................</p>
+
+<p>11. Name of the School last attended : ..............................................................................................................</p>
+
+<p>12. Details of Marks Obtained in Last Examination :</p>
+
+<p>&nbsp; &nbsp; &nbsp; a) Name of Exam: ............. &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;b)Name Of the&nbsp;Board/Council/Univ......................................................</p>
+
+<p>&nbsp; &nbsp; &nbsp; c) Roll &nbsp;No.............................................&nbsp;Year Of Passing ...........................................</p>
+
+<p>&nbsp; &nbsp; &nbsp; d) Marks Obtained:&nbsp;</p>
+
+<table align="center" border="1" cellpadding="1" cellspacing="1" style="width: 700px;">
+	<tbody>
+		<tr>
+			<td rowspan="2" style="text-align: center;">Subject</td>
+			<td style="text-align: center;">Vernacular</td>
+			<td rowspan="2" style="text-align: center;">English</td>
+			<td colspan="2" rowspan="1" style="text-align: center;">&nbsp;</td>
+			<td colspan="2" rowspan="1" style="text-align: center;">&nbsp;</td>
+			<td colspan="2" rowspan="1" style="text-align: center;">&nbsp;</td>
+			<td colspan="2" rowspan="1" style="text-align: center;">&nbsp;</td>
+			<td rowspan="2" style="text-align: center;">ENVS</td>
+		</tr>
+		<tr>
+			<td style="text-align: center;">&nbsp;</td>
+			<td style="text-align: center;">Th.</td>
+			<td style="text-align: center;">Pr.</td>
+			<td style="text-align: center;">Th.</td>
+			<td style="text-align: center;">Pr.</td>
+			<td style="text-align: center;">Th.</td>
+			<td style="text-align: center;">Pr.</td>
+			<td style="text-align: center;">Th.</td>
+			<td style="text-align: center;">Pr.</td>
+		</tr>
+		<tr>
+			<td style="text-align: center;">Full Marks</td>
+			<td style="text-align: center;">&nbsp;</td>
+			<td style="text-align: center;">&nbsp;</td>
+			<td style="text-align: center;">&nbsp;</td>
+			<td style="text-align: center;">&nbsp;</td>
+			<td style="text-align: center;">&nbsp;</td>
+			<td style="text-align: center;">&nbsp;</td>
+			<td style="text-align: center;">&nbsp;</td>
+			<td style="text-align: center;">&nbsp;</td>
+			<td style="text-align: center;">&nbsp;</td>
+			<td style="text-align: center;">&nbsp;</td>
+			<td style="text-align: center;">&nbsp;</td>
+		</tr>
+		<tr>
+			<td style="text-align: center;">Marks Obtained</td>
+			<td style="text-align: center;">&nbsp;</td>
+			<td style="text-align: center;">&nbsp;</td>
+			<td style="text-align: center;">&nbsp;</td>
+			<td style="text-align: center;">&nbsp;</td>
+			<td style="text-align: center;">&nbsp;</td>
+			<td style="text-align: center;">&nbsp;</td>
+			<td style="text-align: center;">&nbsp;</td>
+			<td style="text-align: center;">&nbsp;</td>
+			<td style="text-align: center;">&nbsp;</td>
+			<td style="text-align: center;">&nbsp;</td>
+			<td style="text-align: center;">&nbsp;</td>
+		</tr>
+	</tbody>
+</table>
+
+<p>13. Subjects Applied For:</p>
+
+<table align="center" border="1" cellpadding="1" cellspacing="1" style="width: 500px;">
+	<tbody>
+		<tr>
+			<td>&nbsp;Honours/General</td>
+			<td>Subjects Taken</td>
+			<td colspan="3" rowspan="1">Compulsory</td>
+		</tr>
+		<tr>
+			<td>&nbsp;</td>
+			<td>&nbsp;</td>
+			<td>ENGC</td>
+			<td>BNGMor HINM</td>
+			<td>ENVS</td>
+		</tr>
+	</tbody>
+</table>
+
+<p>14 . Bank Transaction No/Jurnal No/Id- .............................. &nbsp; &nbsp;b)Transaction Date-..............</p>
+
+<p><span style="text-align: right;">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Full Signature of the Candidate with date</span></p>
+
+<div style="page-break-after: always"><span style="display: none;">&nbsp;</span></div>
+
+<p>15. Whether belongs to BPL Family:....................................................................<span style="font-size:9px;">(Attested copy of BPL card should be enclosed)</span></p>
+
+<p>16. Extra Curricular Activities : ........................................................................................................................</p>
+
+<p>17.Game &amp; Sports with Level :.........................................................................................................................</p>
+
+<p>18.Name of Local Guardian :.............................................................................................................................</p>
+
+<p>19.Address of Local Guardian :.........................................................................................................................</p>
+
+<p>20. Whether receive any Scholarship /Financial Assistance (if yes,give details)..............................................</p>
+
+<p>......................................</p>
+
+<p>21. If you previously admitted in this College, state :</p>
+
+<p>&nbsp;Year ...........................................................Class.............,,,,,,,,,,,,,,...................... ....................................&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Roll...................................................... University Regn. No. with year ................................................</p>
+
+<p style="text-align: center;"><span style="color:#FFFFFF;"><strong><span style="background-color:#000000;">GUARDIAN&#39;S DECLARATION</span></strong></span></p>
+
+<p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;I hereby undertake to defray all educational expenses of my Son /Daughter/ Ward &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Sri/Smt./Kumari ..............................................................and promice that he /she shall abide by college regulations and discipline in force or that may be in force time to time .</p>
+
+<p style="text-align: justify;">&nbsp;</p>
+
+<p>Date:&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Full Signature of the&nbsp;Guardian</p>
+
+<p>&nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<span style="color:#FFFFFF;"><strong><span style="background-color:#000000;">DECLARATION BY CANDIDATE</span></strong></span></p>
+
+<p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; I hereby declear that I shall abide by all the Rules &amp; Regulations of the College &nbsp;and Vidyasagar University. I also declare that &nbsp;I shall have no objection if I am debarred from filling up the form for University Examination, for faliling to secure at least 75% class attendance as per V.U. Circular No. VU/IC/CIR/108/97-98 dated 17 July 1997.</p>
+
+<p>Date . &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Full Signature of the Candidate</p>
+
+<table align="center" border="1" cellpadding="1" cellspacing="1" style="width: 500px;">
+	<tbody>
+		<tr>
+			<td>
+			<p>Remarks&nbsp;</p>
+			</td>
+			<td colspan="2">
+			<p>May be admitted</p>
+			</td>
+		</tr>
+		<tr>
+			<td>
+			<p>&nbsp;</p>
+			</td>
+			<td>
+			<p>Dealing Staff Admission Counter&nbsp;</p>
+
+			<p>Kharagpur College</p>
+			</td>
+			<td>
+			<p>Principal /Teacher in Charge</p>
+
+			<p>Kharagpur College</p>
+			</td>
+		</tr>
+	</tbody>
+</table>
+
+<p>&nbsp;</p>
+
+<p><b>&nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</b></p>
+
+<p><b>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</b></p>
+
+<p>&nbsp;</p>
+
+<p>&nbsp;</p>
+
+<p>&nbsp;</p>
+
+<p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</p>
+</body>
+</html>
+';
+$mpdf= new mPDF();
+//$mpdf=new mPDF('win-1252','A6','','',20,15,48,25,10,10); 
+$mpdf = new mPDF('', 'Legal', 0, '', 12.7, 12.7, 14, 12.7, 8, 8);
+//$mpdf->useOnlyCoreFonts = true;    // false is default
+//$mpdf->SetProtection(array('print'));
+$mpdf->SetTitle("Application");
+$mpdf->use_kwt = true;
+$mpdf->shrink_tables_to_fit;
+//$mpdf->SetAuthor("..........");
+//$mpdf->SetWatermarkText("Submitted");
+//$mpdf->showWatermarkText = false;
+//$mpdf->watermark_font = 'DejaVuSansCondensed';
+//$mpdf->watermarkTextAlpha = 0.1;
+
+
+
+
+$mpdf->SetDisplayMode('fullpage');
+//$mpdf->SetHeader('Referenceno-{DATE jmYmmsshh}{PAGENO}');
+//$mpdf->SetFooter('|{PAGENO}|');
+
+//$mpdf->WriteHTML($style,1);
+//$mpdf->WriteHTML($style2,1);
+$mpdf->WriteHTML($html);
+//$mpdf->WriteHTML('hi');
+
+
+//$mpdf->Output($filename,'F');//for saving in current directory fource fully  for letter use 
+//$mpdf->Output($filename,'D');//downlode in browser
+//echo $html;
+$mpdf->Output();//For direct out put in browser
+//echo "jjjjjj";
+/*echo $html;
+$link="<script type=\"text/javascript\">
+'''window.location.href=\"firstpage.html\";'''
+window.open('education.php','_self',false) 
+</script>";
+echo $link;
+*/
+//header("Location:after_submit.php?merit[]=$merit&hons[]=$hons&session=$session");
+exit();
+
+?>
